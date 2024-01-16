2674ae217569ecb21821f43478989d4f19a2d3fc
alamin175 <mdalaminh052@gmail.com>
2024-01-16 11:40:34 +0600

    delete from cart api created

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
   res.send("restaurant data are coming");
 });
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0ids2q5.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,11 +31,46 @@ async function run() {
     const menuCollection = client
       .db("bistro-restaurant")
       .collection("menu-item");
+    const reviewCollection = client
+      .db("bistro-restaurant")
+      .collection("reviews");
+    const cartCollection = client.db("bistro-restaurant").collection("carts");
 
     app.get("/menu", async (req, res) => {
       const query = {};
       const cursor = menuCollection.find(query);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+
+    // cart collection
+
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
+
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      console.log(email);
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
   } finally {
